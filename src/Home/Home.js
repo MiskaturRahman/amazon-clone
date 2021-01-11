3a2295fc3a3e28542fced1149da63e98e76df27f
Misk007 <mdrahmanmiskatur@gmail.com>
2021-01-11 21:51:16 +0600

    Homedone

--- src/Home/Home.js
+++ src/Home/Home.js
@@ -1,4 +1,7 @@
 import React from 'react';
+import './Home.css'
+import Carousel from 'react-bootstrap/Carousel';
+
 import carousel0 from '../images/intro0.png';
 import carousel1 from '../images/intro1.jpg';
 import carousel2 from '../images/intro2.jpg';
@@ -9,38 +12,70 @@ import carousel6 from '../images/intro6.jpg';
 import carousel7 from '../images/intro7.jpg';
 function Home() {
 
-    const carousel = [
-        {
-            img: carousel0
-        },
-        {
-            img: carousel1
-        },
-        {
-            img: carousel2
-        },
-        {
-            img: carousel3
-        },
-        {
-            img: carousel4
-        },
-        {
-            img: carousel5
-        },
-        {
-            img: carousel6
-        },
-        {
-            img: carousel7
-        }
-
-    ]
-
     return (
-        <div>
-            <h1>Home</h1>
-        </div>
+
+        <section className="home">
+            <Carousel>
+                <Carousel.Item interval={2000}>
+                    <img
+                        className="home__image d-block w-100"
+                        src={carousel0}
+                        alt="First slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item interval={2000}>
+                    <img
+                        className="home__image d-block w-100"
+                        src={carousel1}
+                        alt="Third slide"
+                    />
+
+                </Carousel.Item>
+                <Carousel.Item interval={2000}>
+                    <img
+                        className="home__image d-block w-100"
+                        src={carousel2}
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item interval={2000}>
+                    <img
+                        className="home__image d-block w-100"
+                        src={carousel3}
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item interval={2000}>
+                    <img
+                        className="home__image d-block w-100"
+                        src={carousel4}
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item interval={2000}>
+                    <img
+                        className="home__image d-block w-100"
+                        src={carousel5}
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item interval={2000}>
+                    <img
+                        className="home__image d-block w-100"
+                        src={carousel6}
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+                <Carousel.Item interval={2000}>
+                    <img
+                        className="home__image d-block w-100"
+                        src={carousel7}
+                        alt="Third slide"
+                    />
+                </Carousel.Item>
+            </Carousel>
+        </section>
+
     )
 }
 
