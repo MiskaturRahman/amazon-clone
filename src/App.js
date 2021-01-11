15a27969d8321f8b6d69eeaed30cbe70cd7a9280
Misk007 <mdrahmanmiskatur@gmail.com>
2021-01-11 10:54:50 +0600

    update

--- src/App.js
+++ src/App.js
@@ -1,24 +1,33 @@
-import logo from './logo.svg';
+import React from "react";
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 import './App.css';
+import Header from "./Header/Header";
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+
+        <Switch>
+          <Route path="/checkout">
+            <h1>Hello Its ema-john checkout</h1>
+          </Route>
+          <Route path="/login">
+            <h1>Login</h1>
+          </Route>
+          <Route path="/">
+            <Header />
+            <h1>Hello Its ema-john Ecommerce site</h1>
+          </Route>
+        </Switch>
+      </div>
+    </Router>
   );
 }
 
---
+++ src/Header/Header.js
@@ -0,0 +1,74 @@
+import React from 'react'
+import logo from '../images/logo.png';
+import './Header.css';
+import {
+    BrowserRouter as Router,
+    Switch,
+    Route,
+    Link
+} from "react-router-dom";
+import SearchIcon from '@material-ui/icons/Search';
+import ShoppingBasketIcon from '@material-ui/icons/ShoppingBasket';
+
+
+function Header() {
+
+    return (
+        <nav className="header">
+            {/* logo */}
+            <Link to="/">
+                <img className="header__logo" src={logo} alt="as" />
+            </Link>
+
+            {/* search box */}
+            <div className="header__search">
+                <input type="text" className="header__searchInput" />
+                <SearchIcon className="header__searchIcon" />
+            </div>
+
+            {/* 3 links */}
+            <div className="header__nav">
+                {/* 1st link */}
+                <Link to="/login" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Hello Misk</span>
+                        <span className="header__optionLineTwo">Sign In</span>
+                    </div>
+                </Link>
+
+                {/* 2nd link */}
+                <Link to="/" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Returns</span>
+                        <span className="header__optionLineTwo">& Orders</span>
+                    </div>
+                </Link>
+
+                {/* 3rd link */}
+                <Link to="/" className="header__link">
+                    <div className="header__option">
+                        <span className="header__optionLineOne">Your</span>
+                        <span className="header__optionLineTwo">Prime</span>
+                    </div>
+                </Link>
+
+                {/* 4th link*/}
+                <Link to="/checkout" className="header__link">
+                    <div className="header__optionBasket">
+                        {/* icon */}
+                        <ShoppingBasketIcon />
+                        {/* items in basket */}
+                        <span className="header__optionLineTwo header__basketCount">0</span>
+                    </div>
+                </Link>
+
+            </div>
+
+
+            {/* cart basket */}
+
+        </nav>
+    )
+}
+
+export default Header
